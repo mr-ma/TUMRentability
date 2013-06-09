b835607cc6bf98cb9e31bfb835e9518d6b8202c3
Phil S. <philippncsimon@yahoo.com>
2013-06-09 17:19:05 +0200

    Created Basic User Entity

---
+++ Rentability/app/models/User.java
@@ -0,0 +1,40 @@
+package models;
+
+import play.*;
+import play.db.jpa.*;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+public class User extends Model {
+	
+	
+
+	public String first_name;
+	
+	public String last_name;
+	
+	public String email;
+	
+	public String phone;
+
+	// public String password; //MD5 Hash?!
+	
+	public User(String first_name, String last_name, String email, String phone) {
+		super();
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.email = email;
+		this.phone = phone;
+		create();
+	}
+	
+	
+	
+	
+	
+	
+	
+    
+}
