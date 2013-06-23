ef069ab9a00aac06c2ef047386d7548f4a677fc1
Massi1000 <m.altendeitering@tum.de>
2013-06-23 22:15:51 +0200

    Bugfixes to the mail functions. And commented the send function for not creating to much spam xD

--- Rentability/app/tools/Mailing.java
+++ Rentability/app/tools/Mailing.java
@@ -15,62 +15,64 @@ public class Mailing {
 	final static String PASSWORD = "Rentability123";
 	final static int PORT = 465;
 
+	/*After a new Request has been created a confirm mail is send to the requesting User
+	and one is send to the owner.*/
 	public static void newRequest(User requestingUser, Request request) {
 		
 		User owner = request.offer.article.owner;
+		String message;
+		String subject;
 		
-        try {
-        	
-        	//Send E-Mail to owner of the article
-			HtmlEmail email = new HtmlEmail();
-			email.setHostName(SMTP);
-			email.setSmtpPort(PORT);
-			email.setAuthenticator(new DefaultAuthenticator(SENDER, PASSWORD));
-			email.setSSL(true);
-			email.setFrom(SENDER);
-			email.setSubject("New Request for Article: " + request.offer.article.name);
-			email.setHtmlMsg("<html><body><p>Dear User</p></body></html>");
-			email.addTo(owner.email);
-			email.send();
+        //Send E-Mail to owner of the article
+		subject = "A new request for the Article: " + request.offer.article.name + " has been received!";
+		message = "<html><body><p>Dear " + owner.nick_name + "</p>" +
+				"<p>Your Article: " + request.offer.article.name + " has been requested. <br />" +
+				"Please log into your Rentability account for further steps. <br /></p>" +
+				"<p>The Request Details:" +
+				"<br />Start Time: " + request.startTime +
+				"<br />End Time: " + request.endTime +
+				"<br />Requesting User: " + requestingUser.nick_name + 
+				"<br />Adjusted Price: " + request.adjustedPrice + "</p>" +
+				"<br /><br />Your Rentability Team!</body></html>";
+		//sendMail(owner.email, subject, message);
 			
-			//Send confirmation E-Mail to the requester
-			HtmlEmail email2 = new HtmlEmail();
-			email2.setHostName(SMTP);
-			email2.setSmtpPort(PORT);
-			email2.setAuthenticator(new DefaultAuthenticator(SENDER, PASSWORD));
-			email2.setSSL(true);
-			email2.setFrom(SENDER);
-			email2.setSubject("Your Request has sucessfully been created!");
-			email2.setHtmlMsg("<html><body><p>Dear " + requestingUser.nick_name + ",</p><p>" +
-					"your request has successfully been created! Log into your Account for details.</p>" +
-					"<p>Your Rentability Team!</p></body></html>");
-			email2.addTo(owner.email);
-			email2.send();
-		} catch (EmailException e) {
-			e.getMessage();
-		}	
+		//Send confirmation E-Mail to the requester
+		subject = "Your Request has successfully been created!";
+		message = "<html><body><p>Dear " + requestingUser.nick_name + ",</p><p>" +
+				"your request has successfully been created! Log into your Account for details.</p>" +
+				"<p>Your Rentability Team!</p></body></html>";
+		//sendMail(requestingUser.email, subject, message);
 	}
 
+	//Send the confirmation Mail to the user for registration
 	public static void sendConfirmationMail(User user) {
+		Map<String, Object> args = new HashMap<String, Object>();
+		args.put("code", user.confirmationCode);
+		String confirmation_link = Router.getFullUrl("Application.confirm", args);
+		String subject = "Please verify your account";
+		String message = "<html><body><p>Dear " + user.nick_name + "" +
+				",</p><p>please verify your account by clicking the link below:</p>" +
+				"<p><a href=\""+ confirmation_link + "\">" + confirmation_link + "</a></p>" +
+				"<p>Thanks! Your Rentability Team!</p></body></html>";
+		
+		//sendMail(user.email, subject, message);
+	}
+	
+	//Technical stuff to actually send the mail
+	private static void sendMail(String recipient, String subject, String htmlMessage){
 		try {
-			Map<String, Object> args = new HashMap<String, Object>();
-			args.put("code", user.confirmationCode);
-			String confirmation_link = Router.getFullUrl("Application.confirm", args);
 			HtmlEmail email = new HtmlEmail();
 			email.setHostName(SMTP);
 			email.setSmtpPort(PORT);
 			email.setAuthenticator(new DefaultAuthenticator(SENDER, PASSWORD));
 			email.setSSL(true);
 			email.setFrom(SENDER);
-			email.setSubject("Verify your Rentability account");
-			email.addTo(user.email);
-			email.setHtmlMsg("<html><body><p>Dear " + user.nick_name + "" +
-					",</p><p>please verify your account by clicking the link below:</p>" +
-					"<p><a href=\""+ confirmation_link + "\">" + confirmation_link + "</a></p>" +
-							"<p>Thanks! Your Rentability Team!</p></body></html>");
+			email.setSubject(subject);
+			email.addTo(recipient);
+			email.setHtmlMsg(htmlMessage);
 			email.send();
-		} catch (EmailException e) {
-			e.printStackTrace();
+		} catch (EmailException emailex) {
+			System.out.println(emailex.getMessage());
 		}
 	}
 }
