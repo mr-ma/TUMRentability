530ad13d8c93013d985b07db80087d96d795d7a8
Mohsen Ahmadvand <mohsen.ahmadv@gmail.com>
2013-06-30 11:18:12 +0200

    Merging userProfile

--- Rentability/app/controllers/Rentability.java
+++ Rentability/app/controllers/Rentability.java
@@ -4,7 +4,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-import javax.validation.*;
 
 import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.Email;
@@ -213,5 +212,186 @@ public class Rentability extends Controller {
     	render("@userProfile");
     }
 
+    public static void userArticles()
+    {
+    	if(Security.isConnected()) {
+            User user = User.find("byEmail", Security.connected()).first();
+            List<Article> userArticles=  Article.find("owner.id", user.id).fetch();
+            renderArgs.put("articles", userArticles);
+            render();
+    	}
+    }
     
+    public static void articleOffers(long articleId)
+    {
+    	Article article= Article.findById(articleId);
+    	List<Offer> offers= Article.getAllOffers(article);
+    	renderArgs.put("article", article);
+    	renderArgs.put("offers", offers);
+    	render();
+    }
+    
+  public static void offerRequests(long offerId)
+  {
+	Offer offer= Offer.findById(offerId);
+  	List<Request> requests= Offer.getAllRequests(offer);
+  	boolean alreadyApproved=false;
+  	for(int i=0;i<requests.size();i++)
+  	{
+  	if(requests.get(i).state ==5) alreadyApproved=true; 
+  	}
+  	renderArgs.put("alreadyApproved",alreadyApproved);
+  	renderArgs.put("offer", offer);
+  	renderArgs.put("requests", requests);
+  	render();
+  }
+  
+  public static void approveRequest(long requestId,long offerId)
+  {
+	  Request request= Request.findById(requestId);
+	  request.state=5;
+	  request.save();
+	  Offer offer= Offer.findById(offerId);
+	  offer.state= -1;
+	  offerRequests(offerId);
+	  
+  }
+  
+//Rendering the create offer page using all existing categories
+  public static void createOfferWithArticle(long articleId) {
+  	List<Category> categories = Category.findAll();
+  	Article article= Article.findById(articleId);
+  	session.put("article", articleId);
+  	renderArgs.put("article", article);
+  	render(categories);
+  }
+  //Creating a new Offer
+  public static void saveOfferOfArticle(String description, String name, 
+  		String pickUpAddress, String startTime, String endTime, String price, String insurance) {    	
+  	
+	  
+	Article article =Article.findById(Long.parseLong(session.get("article"))); 
+  	validation.required(description);
+  	validation.required(pickUpAddress);
+  	validation.required(startTime);
+  	//Make sure the date is entered in this format DD.MM.YYYY
+  	validation.match(startTime, "\\d{2}\\.\\d{2}\\.\\d{4}").message("Please indicate the Date in the given format!");
+  	validation.required(endTime);
+  	validation.match(endTime, "\\d{2}\\.\\d{2}\\.\\d{4}").message("Please indicate the Date in the given format!");
+  	validation.required(price);
+  	//Ensures that the price field is followed by 2 digits after the point
+  	validation.match(price, "\\d+\\.\\d{2}").message("Please indicate the Price in the given format!");
+  	
+  	if(validation.hasErrors())
+  	{
+  		List<Category> categories = Category.findAll();
+  		render("@createOfferWithArticle" ,article, description, categories, pickUpAddress,
+  				startTime, endTime, price, insurance);
+  	}
+  	else
+  	{
+  		boolean insuranceRequired;
+      	//Conversion of String Values to Dates, Boolean, etc.
+      	SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+			try {
+				Date start = sdf.parse(startTime);
+				Date end = sdf.parse(endTime);
+				
+				if(insurance == null || !insurance.equals("true"))
+					insuranceRequired = false;
+				else
+					insuranceRequired = true;
+				
+				double doublePrice = Double.parseDouble(price);
+				
+				//null value to be implemented - represents the description
+	        	new Offer(pickUpAddress, insuranceRequired, 0, doublePrice, description, start, end, article);
+	        	
+	        	flash.success("Your offer has successfully been created!");
+	        	Application.index();
+	        	
+			} catch (Exception ex) {
+				flash.error("Sorry an error occured, please try again!");
+			}
+  	}
+  }
+
+  public static void removeOffer(long offerId,long articleId)
+  {
+	  //if offer has requests can not be deleted
+		Offer offer= Offer.findById(offerId);
+		int requestssize=offer.getAllRequests(offer).size();
+		if(requestssize>0)
+		{
+			flash.error("This offer has "+requestssize +" rent requests(s)! Can not be removed" );
+		}
+		else
+		{
+		///remove offer
+			offer.delete();
+		}
+			
+		articleOffers(articleId); 
+  }
+  public static void removeArticle(long articleId)
+  {
+	  //if article has offer can not be deleted
+		Article article= Article.findById(articleId);
+		int offerssize=Article.getAllOffers(article).size();
+		if(offerssize>0)
+		{
+			flash.error("This article has "+offerssize +" offer(s)! First remove offers in order to be able remove the article" );
+		}
+		else
+		{
+		///remove article
+			article.delete();
+		}
+		userArticles();
+  }
+  
+  
+  ///Provide requests of current user and their state approved or pending
+  public static void userRequests()
+  {
+	  if(Security.isConnected()) {
+          User user = User.find("byEmail", Security.connected()).first();
+          List<Request> userRequest=  Request.find("user.id", user.id).fetch();
+          renderArgs.put("requests", userRequest);
+          render();
+  	}
+  }
+  
+  
+  public static void changePassword()
+  {
+	  
+	  render();
+  }
+  
+  public static void updatePassword(@Required String currentpassword,@Required String newpassword,@Required String verifypassword)
+  {
+		validation.required(currentpassword).message("Field is required");
+    	validation.required(newpassword).message("Field is required");
+    	validation.required(verifypassword).message("Field is required");
+    	 validation.equals(verifypassword, newpassword).message("Your password doesn't match");
+    	   if(validation.hasErrors()){
+        	render("@changePassword",currentpassword);
+    	   }
+            else
+            {
+            	User user = User.find("byEmail", Security.connected()).first();     
+            	if( user.password.equals(currentpassword)) {
+            		user.password= newpassword;
+            		user.save();
+            		flash.success("Successfully updated your password");
+            		changePassword();
+            	}
+            	else
+            	{
+            		flash.error("Entered password is not correct ");
+            		render("@changePassword",currentpassword);
+            	}
+            }
+  }
 }
